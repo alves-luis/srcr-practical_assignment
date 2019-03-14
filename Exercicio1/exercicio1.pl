2bf4794d1aefc6b1edcfbd7a1efa1f0e010dfc66
Luís Alves <24353152+alves-luis@users.noreply.github.com>
2019-03-14 15:33:19 +0000

    Grande update
    Provavelmente está quase pronto para entrega, falta testar, adicionar mais conhecimento e verificar se falta algo. E relatório, claro

--- Exercicio1/exercicio1.pl
+++ Exercicio1/exercicio1.pl
@@ -18,10 +18,11 @@
 :- dynamic utente/4.
 :- dynamic servico/4.
 :- dynamic consulta/4.
+:- dynamic sexo/2.
 
-utente(1, 'André', 20, 'Espanha').
-utente(2, 'João', 20, 'Braga').
-utente(3, 'Luís', 21, 'Braga').
+utente(1, 'Andre', 20, 'Espanha').
+utente(2, 'Joao', 20, 'Braga').
+utente(3, 'Luis', 21, 'Braga').
 utente(4, 'Rafaela', 20, 'Braga').
 
 sexo('M',1).
@@ -30,25 +31,29 @@ sexo('M',3).
 sexo('F',4).
 
 servico(1, 'Ortodontia', 'Hospital', 'Braga').
-servico(2, 'Medicina Geral', 'Clínica', 'Braga').
+servico(2, 'Medicina Geral', 'Clinica', 'Braga').
 servico(3, 'Oftalmologia', 'Privado', 'Barcelos').
 servico(4, 'Ortopedia', 'Hospital', 'Lisboa').
 
 consulta('19/3/19', 1, 3, 25).
 consulta('23/4/19', 2, 1, 3).
-consulta('3/3/19', 3, 3, 33).
+consulta('3/3/19', 1, 3, 33).
 consulta('7/6/19', 3, 2, 15).
 consulta('7/6/19', 1, 2, 15).
 
-%Questão 1
+% Questão 1 - Registar utentes, serviços e consultas;
 
-  % Extensao do predicado registarU: IdUtente, NomeUtente, Idade, Cidade -> {V,F}
-  % Só pode registar se não existir utente com o mesmo ID
-  registarU(ID, N, I, C) :- nao(utente(ID,_,_,_)),
-    assert(utente(ID,N,I,C)).
+  % Extensao do predicado registarU: IdUtente, NomeUtente, Idade, Cidade, Sexo -> {V,F}
+  % Só pode registar se não existir utente com o mesmo ID e sexo for válido
+  registarU(ID, N, I, C, S) :- nao(utente(ID,_,_,_)),
+    nao(sexo(S,ID)),
+    assert(utente(ID,N,I,C)),
+    assert(sexo(S,ID)).
   % Extensao do predicado registarS: IdServico, Descricao, Instituicao, Cidade -> {V,F}
-  % Só pode registar serviço se não existir serviço com o mesmo ID
-  registarS(ID,D,I,C) :- nao(servico(A,_,_,_)),
+  % Só pode registar serviço se não existir serviço com o mesmo ID e se não existir
+  % serviço com o mesmo nome na mesma instituição
+  registarS(ID,D,I,C) :- nao(servico(ID,_,_,_)),
+    nao(servico(_,D,I,_)),
     assert(servico(ID,D,I,C)).
   % Extensao do predicado registarC: Data, IdUtente, IdServico, Custo -> {V,F}
   % Só pode registar consulta se não existir para o mesmo serviço no mesmo Dia
@@ -60,12 +65,16 @@ consulta('7/6/19', 1, 2, 15).
     servico(IdS,_,_,_),
     assert(consulta(D,IdU,IdS,C)).
 
-%Questão 2
+% Questão 2 - Remover utentes, serviços e consultas;
 
   % Extensao do predicado apagarU: IdUtente -> {V,F}
   % Só pode remover utente se existir o utente e não existirem consultas
-  % Com esse utente.
-  apagarU(ID) :- utente(ID,_,_,_), nao(consulta(_,ID,_,_)), retract(utente(ID,_,_,_)).
+  % Com esse utente e existir registo do seu sexo.
+  apagarU(ID) :- utente(ID,_,_,_),
+    nao(consulta(_,ID,_,_)),
+    sexo(_,ID),
+    retract(utente(ID,_,_,_)),
+    retract(sexo(_,ID)).
   % Extensao do predicado apagarS: IdServico -> {V,F}
   % Só pode remover serviço se existir o serviço e não existirem consultas
   % Desse serviço
@@ -75,30 +84,28 @@ consulta('7/6/19', 1, 2, 15).
   apagarC(D,ID,IDS) :- consulta(D,ID,IDS,_), retract(consulta(D,ID,IDS,_)).
 
 
-%Questão 3
-  %Extensao do predicado TodasInst: Lista -> {V,F}
+% Questão 3 - Identificar as instituições prestadoras de serviços;
+  %Extensao do predicado todasInst: Lista -> {V,F}
   todasInst(S) :- solucoes(Inst,servico(_,_,Inst,_),X),
                   semRepetidos(X,S).
 
 
-%Questão 4
+% Questão 4 - Identificar utentes/serviços/consultas por critérios de seleção;
   % Extensao do predicado utentePorSexo: Sexo do Utente, Lista de Utentes  -> {V,F}
-  utentePorSexo(S,L) :- solucoes(Id,sexo(S,Id),L).
+  utentePorSexo(Sexo,L) :- solucoes(Nome, (sexo(Sexo,Id),utente(Id,Nome,_,_) ),L).
   % Extensao do predicado utentePorCidade: Cidade do Utente, Lista de Utentes  -> {V,F}
   utentePorCidade(C,L) :- solucoes(Nome,utente(_,Nome,_,C),L).
   % Extensao do predicado utentePorIdade: Idade do Utente, Lista de Utentes  -> {V,F}
   utentePorIdade(I,L) :- solucoes(Nome,utente(_,Nome,I,_),L).
 
-  % Extensao do predicado servicosEmCidade: Cidade, Lista de Servicos  -> {V,F}
-  servicosEmCidade(C,L):- solucoes(S, servico(_,S,_,C),L).
+
   % Extensao do predicado todosServicos: Lista de Servicos  -> {V,F}
   todosServicos(L):- solucoes(S, servico(_,S,_,_),L).
-  % Extensao do predicado servicoInstituicaoLocal: Instituicao, Lista de Servicos  -> {V,F}
-  servicoInstituicaoLocal(I,L):- solucoes((C,S), servico(_,S,I,C),L).
 
   % Extensao do predicado consultasPorDia: Dia, Lista de Consultas  -> {V,F}
-  consultasPorDia(D,L) :- solucoes(Id , consulta(D,Id,_,_) , X),
-                         getId(X,L).
+  % Identifica o par Nome de Utente, Descrição do Serviço efetuadas num dado dia.
+  % Assume-se que ninguém efetua mais que uma consulta do mesmo serviço por dia.
+  consultasPorDia(D,L) :- solucoes((Nome,Desc), (consulta(D,IdU,IdS,_) , utente(IdU,Nome,_,_), servico(IdS,Desc,_,_)) , L).
   % Extensao do predicado consultasServiço: Data, Serviço, NumConsultas  -> {V,F}
   consultasServico(D,S,N) :-  servico(IdS,S,_,_),
                               solucoes(Id , consulta(D,Id,IdS,_), L),
@@ -106,17 +113,57 @@ consulta('7/6/19', 1, 2, 15).
   % Extensao do predicado ganhoPorDia: Data, Valor  -> {V,F}
   ganhoPorDia(D,T) :-  solucoes(Valor , consulta(D,_,_,Valor), L),
                        sumElements(L,T).
-%Questão 5
 
-%Questão 6
+% Questão 5 - Identificar serviços prestados por instituição/cidade/datas/custo;
 
-%Questão 7
+  % Extensao do predicado servicosPorCidade: Cidade, Lista de Servicos  -> {V,F}
+  servicosPorCidade(C,L) :- solucoes(S, servico(_,S,_,C),R), semRepetidos(R,L).
 
-%Questão 8
+  % Extensao do predicado servicosPorData: Data, Lista de Servicos  -> {V,F}
+  % Identifica os serviços para os quais foram registadas consultas numa dada data.
+  servicosPorData(Data,L) :- solucoes(Desc, (consulta(Data,_,IdServico,_) , servico(IdServico,Desc,_,_)),R),
+                          semRepetidos(R,L).
 
+  % Extensão do predicado servicosPorCusto: Custo, Lista de Serviços -> {V,F}
+  % Identifica os serviços para os quais foram registadas consultas com um dado custo
+  servicosPorCusto(Custo,L) :- solucoes(Desc, (consulta(_,_,IdServico,Custo) , servico(IdServico,Desc,_,_)), R),
+                          semRepetidos(R,L).
 
+%Questão 6 - Identificar os utentes de um serviço/instituição;
 
+  % Extensão do predicado utentesPorServico: Serviço, Lista de Utentes -> {V,F}
+  % Identifica os utentes para os quais já foi prestado um dado serviço
+  utentesPorServico(IdS,L) :- solucoes(Nome, (consulta(_,IdU,IdS,_) , servico(IdS,_,_,_) , utente(IdU,Nome,_,_)), R),
+                          semRepetidos(R,L).
 
+  % Extensão do predicado utentesPorInstituicao: Instituicao, Lista de Utentes -> {V,F}
+  % Identifica os utentes para os quais já foram prestados serviços numa dada instituição
+  utentesPorInstituicao(Ins,L) :- solucoes(Nome, (consulta(_,IdU,IdS,_) , servico(IdS,_,Ins,_) , utente(IdU,Nome,_,_)), R),
+                            semRepetidos(R,L).
+
+%Questão 7 - Identificar serviços realizados por utente/instituição/cidade;
+
+  % Extensão do predicado servicosPorUtente: Utente, Lista de Serviços -> {V,F}
+  % Identifica os serviços realizados por um utente
+  servicosPorUtente(IdU,L) :- solucoes(Desc, (consulta(_,IdU,IdS,_) , servico(IdS, Desc,_,_)), R),
+                            semRepetidos(R,L).
+
+  % Extensão do predicado servicosPorUtente: Instituicao, Lista de Serviços -> {V,F}
+  % Identifica os serviços realizados por um utente
+  servicosPorInstituicao(Inst,L) :- solucoes(Desc, (consulta(_,IdU,IdS,_) , servico(IdS, Desc,_,_)), R),
+                            semRepetidos(R,L).
+
+  % Extensão do predicado servicosUIC: IdUtente, Instituicao, Cidade, Lista de Serviços -> {V,F}
+  % Identifica os serviços realizados ou por um dado id de utente, numa dada instituição ou cidade
+  servicosUIC(IdU,Inst,Cid,L) :- solucoes((Nome,Desc,Inst,Cid) , (consulta(_,IdU,IdS,_), servico(IdS,Desc,Inst,Cid), utente(IdU,Nome,_,_)), R),
+                              semRepetidos(R,L).
+
+%Questão 8 - Calcular o custo total dos cuidados de saúde por utente/serviço/instituição/data
+
+  % Extensão do predicado custoUSID: IdUtente, Serviço, Instituicao, Data, Custo Total -> {V,F}
+  % Calcula o valor gasto dado um Id de utente, serviço, uma instituição e/ou data
+  custoUSID(IdU,IdS,Inst,Data,R) :- solucoes(C, (consulta(Data,IdU,IdS,C) , servico(IdS,_,Inst,_)), L) ,
+                                sumElements(L,R).
 
 
 % Extensão do predicado pertence: Elemento, Lista -> {V,F}
@@ -130,8 +177,8 @@ nao(T).
 
 % Extensão do predicado solucoes: Formato, Prova, LSolucoes -> {V,F}
 %solucoes(F,P,L) :- findall(F,P,L).
-solucoes(F,P,S) :- P , assert(tmp(F)), fail.
-solucoes(F,P,S) :- construir(S,[]).
+solucoes(F,P,L) :- P , assert(tmp(F)), fail.
+solucoes(F,P,L) :- construir(L,[]).
 
 % Extensão do predicado construir: Solucao, Lista-> {V,F}
 construir(S,L) :- retract(tmp(X)), ! ,construir(S,[X|L]).
@@ -141,10 +188,6 @@ construir(S,S).
 semRepetidos([],[]).
 semRepetidos([H|T],S) :-  pertence(H,T) , semRepetidos(T,S).
 semRepetidos([H|T],[H|S]) :- nao(pertence(H,T)) , semRepetidos(T,S).
-
-% Extensão do predicado getId:: ListaId, ListaNomes -> {V,F}
-getId([],[]).
-getId([H1|C1],[N|C]) :- utente(H1,N,_,_), getId(C1,C).
 
 % Extensão do predicado countElements:: ListaId, NumConsultas -> {V,F}
 countElements([],0).
