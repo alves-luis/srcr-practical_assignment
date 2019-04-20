9b7a9ef3346c4489af6f586413ae50767ef57d88
Luís Alves <24353152+alves-luis@users.noreply.github.com>
2019-04-20 11:24:55 +0100

    Adicionado melhor sistema de inferência e evolução de conhecimento positivo quando é perfeito e imperfeito

--- Exercicio2/exercicio2.pl
+++ Exercicio2/exercicio2.pl
@@ -12,6 +12,10 @@
 :- dynamic utente/4.
 :- dynamic prestador/4.
 :- dynamic cuidado/5.
+:- dynamic '-'/1.
+:- dynamic excecao/1.
+:- dynamic impreciso/1.
+:- dynamic incerto/1.
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Conhecimento Perfeito Positivo
@@ -30,7 +34,23 @@ utente( 15 , 'Joao Miguel Abreu' , 35, 'Rua das Amoreiras' ).
 +utente(Id,Nome,Idade,Morada) :: (
     solucoes(Id, utente(Id,Nome,Idade,Morada),L),
     comprimento(L,N),
-    N == 1
+    N == 0
+).
+
+% Invariante Estrutural
+% Não permitir a inserção de conhecimento negativo repetido
++(-utente(Id,Nome,Idade,Morada)) :: (
+    solucoes(Id, -utente(Id,Nome,Idade,Morada), L),
+    comprimento(L,N),
+    N == 0
+).
+
+% Invariante estrutural
+% não permitir a inserção de conhecimento contraditório
++utente(Id,Nome,Idade,Morada) :: (
+    solucoes((Id,Nome,Idade,Morada), -utente(Id,Nome,Idade,Morada), L),
+    comprimento(L,N),
+    N =< 1
 ).
 
 % Não permitir a inserção de utentes caso seja interdito adicionar a idade
@@ -65,8 +85,20 @@ prestador( 9 , 'Diogo Teixeira' , 'Cardiologia' , 'Hospital de Faro').
 +prestador(Id,Nome,Especialidade,Instituicao) :: (
     solucoes(Id, prestador(Id,Nome,Especialidade,Instituicao), L),
     comprimento(L,N),
-    N == 1
+    N == 0
 ).
+
+% Invariante estrutural
+% Não permitir a inserção de conhecimento contraditório
++prestador(Id,Nome,Especialidade,Instituicao) :: (
+    solucoes(Id, -prestador(Id,Nome,Especialidade,Instituicao), L),
+    comprimento(L,N),
+    N =< 1
+).
+
+% Invariante estrutural
+% Não é possível adicionar prestadores de Especialidades interditas.
++prestador(Id,Nome,Especialidade,Instituicao) :: (nao(interdito(Especialidade))).
 
 % Extensão do predicado cuidado: Data, Id Utente, Id Prestador, Descrição, Custo -> {V,F}
 cuidado(data(5,10,2018) , 0, 1 , 'cuidado ao Coracao' , 20).
@@ -90,7 +122,7 @@ cuidado(data(18,6,2010),3,6,'cuidado ao Coracao' ,30).
 % Não permitir a inserção de conhecimento repetido de cuidados
 +cuidado(Data,IdU,IdS,Descricao,Custo) :: (solucoes(Ls, cuidado(Data,IdU,IdS,Descricao,Custo),S),
                                   comprimento(S,N),
-                                  N == 1).
+                                  N == 0).
 
 % Invariante referencial
 % Não permitir a inserção de novos cuidados se não existir o utente com
@@ -129,19 +161,19 @@ utente( 5,'Joaquina',nulo(incerto),'Av.da Liberdade, Lisboa').
                                        nao(excecao(prestador(Id,Nome,Esp, Inst))).
 
 % O Joaquim da Graça não presta Urologia no Hospital de Guimarães
--prestador( 0 , 'Joaquim da Graca' , 'Urologia' , 'Hospital de Guimaraes').
+-prestador( 41 , 'Joaquim da Graca' , 'Urologia' , 'Hospital de Guimaraes').
 
 % O Miguel Dourado não presta Gastrenterologia em lado algum
--prestador( 4 , 'Miguel Dourado' , 'Gastrenterologia', Instituicao).
+-prestador( 42 , 'Miguel Dourado' , 'Gastrenterologia', Instituicao).
 
 % O Sousa Costa apenas presta cuidados de saúde no Hospital de Faro
--prestador( 6 , 'Sousa Costa' , _ , Instituicao) :-
+-prestador( 43 , 'Sousa Costa' , _ , Instituicao) :-
     nao(Instituicao == 'Hospital de Faro').
 
 % O Marafa da Derme apenas presta Dermatologia no Hospital de Braga
--prestador( 1 , 'Marafa da Derme' , _ , Instituicao) :-
+-prestador( 44 , 'Marafa da Derme' , _ , Instituicao) :-
     nao(Instituicao == 'Hospital de Braga').
--prestador( 1 , 'Marafa da Derme' , Especialidade , _ ) :-
+-prestador( 45 , 'Marafa da Derme' , Especialidade , _ ) :-
     nao(Especialidade == 'Dermatologia').
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
@@ -152,24 +184,29 @@ utente( 5,'Joaquina',nulo(incerto),'Av.da Liberdade, Lisboa').
 % Não se sabe o nome do utente
 utente( 6 , nulo(incerto), 30 , 'Rua dos Pomares' ).
 excecao(utente(Id,Nome,Idade,Morada)) :- utente(Id,nulo(incerto),Idade,Morada).
+incerto(utente(6)).
 
 % Não se sabe a morada do utente Rui Pedro, apesar de se saber a sua idade.
 utente( 7 , 'Rui Pedro', 32 , nulo(incerto) ).
 excecao(utente(Id,Nome,Idade,Morada)) :- utente(Id,Nome,Idade,nulo(incerto)).
+incerto(utente(7)).
 
 % Não se sabe a idade do utente
 utente( 8 , 'Jorge', nulo(incerto) , 'Rua Pinheiral' ).
 excecao(utente(Id,Nome,Idade,Morada)) :- utente(Id,Nome,nulo(incerto),Morada).
+incerto(utente(8)).
 
 % Não se sabe qual a especialidade que o Hugo Anibal presta, uma vez que
 % não foi preenchido o formulário 37423_B aquando da sua entrada a prestador
 % no hospital
 prestador( 10 , 'Hugo Anibal' , nulo(incerto) , 'Hospital da Luz').
 excecao(prestador(Id,Nome,Especialidade,Instituicao)) :- prestador(Id,Nome,nulo(incerto), Instituicao).
+incerto(prestador(10)).
 
 % Não se sabe qual o hospital em que o Andre presta cuidado
 prestador( 11 , 'Andre' , 'Genecologia' , nulo(incerto)).
-excecao(prestador(Id,Nome,Especialidade,Instituicao)) :- prestador(Id,Nome,nulo(incerto), Instituicao).
+excecao(prestador(Id,Nome,Especialidade,Instituicao)) :- prestador(Id,Nome, Especialidade, nulo(incerto)).
+incerto(prestador(11)).
 
 % Não se sabe qual a data em que o cuidado foi prestado
 cuidado(nulo(incerto),4,6,'Consulta breve',30).
@@ -181,6 +218,7 @@ excecao(cuidado( Data, IdUtente, IdPrestador,Descricao, Custo)) :- cuidado(Data,
 
 % -------------------------------
 % Tipo 2 - Conhecimento Impreciso
+% -------------------------------
 
 % Um cuidado pode ter custado 40 ou 60 euros
 excecao(cuidado(data(10,7,2019), 3, 2,'Consulta breve', 40)).
@@ -191,10 +229,10 @@ excecao(cuidado(data(10,7,2019), 3, 2,'Consulta breve', 60)).
 % uma vez que foram encontradas assinaturas com estes dois nomes em vários papéis.
 excecao(prestador( 13 , 'Joao Teixeira' , 'Cardiologia' , 'Hospital de Faro')).
 excecao(prestador( 13 , 'Alberto Ricardo' , 'Cardiologia' , 'Hospital de Faro')).
+impreciso(prestador(13)).
 
 % O utente com o id nº20 tem uma idade compreendida entre 18 e 23
-excecao(utente( 20,'Alexandra',C,'Avenida 25 de Abril','Santarém','935694789')) :- C >= 18 , C =< 25.
-excecao(utente( 16 , 'Miguel Joao' , I, 'Rua Casal Garcia ' )) :- I >= 18 , I =< 23.
+excecao(utente( 20,'Alexandra',I,'Avenida 25 de Abril, Santarem')) :- I >= 18 , I =< 23.
 
 % -------------------------------
 % Tipo 3 - Conhecimento Interdito
@@ -203,64 +241,107 @@ interdito('Homeopatia').
 interdito('Acupuntura').
 interdito('Aromaterapia').
 
-% Invariante estrutural
-% Não é possível adicionar prestadores de Especialidades interditas.
-+prestador(Id,Nome,Especialidade,Instituicao) :: (nao(interdito(Especialidade))).
-
 % cuidado do dia 17-04-2019 tem um utente que ninguém pode conhecer
 cuidado(data(17,04,2019), nulo(interdito), 2, 60).
 
-interdito(3).
-% Invariante estrutural
-% Não é possível adicionar ID utente de determinado cuidado.
-+cuidado( Data, IdUtente, IdPrestador, Custo) :: (nao(interdito(IdUtente))).
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Evolução do conhecimento perfeito positivo
+evolucao( Termo ) :-
+    nao(imperfeito(Termo)),
+    solucoes( Invariante,+Termo::Invariante,Lista ),
+    teste( Lista ),
+    assert(Termo).
+
+% Evolução do conhecimento havendo já conhecimento imperfeito
+evolucao( Termo ) :-
+    imperfeito( Termo ),
+    si(Termo,desconhecido),
+    remocaoIncerto(Termo),
+    solucoes(Invariante,+Termo::Invariante, Lista),
+    teste(Lista),
+    assert(Termo),
+    remocaoImperfeito(Termo).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Evolução do conhecimento
-evolucao( Termo ) :-
-    solucoes( Invariante,+Termo::Invariante,Lista ),
-    insercao( Termo ),
-    teste( Lista ).
+% Extensão do predicado que permite a involucao do conhecimento positivo
+involucao( T ) :- T,
+                solucoes(I, -T::I, LInv),
+                teste(LInv),
+                retract(T).
 
-utentePerfeito(Id,Nome,Idade,Morada) :- nao(Nome == nulo(impreciso)),
-                                        nao(Idade == nulo(impreciso)),
-                                        nao(Morada == nulo(impreciso)),
-                                        nao(Id == nulo(impreciso)).
+involucao(-T) :- -T,
+                solucoes(I,-(-T::I),LInv),
+                teste(LInv),
+                retract(-T).
 
-% THIS BELOW IS A WORK IN PROGRESS
-removeImpreciso(utente(Id,Nome,Idade,Morada)) :-
-    retract(utente(Id,_,_,_)).
+% -----------------
+% Predicados auxiliares para evolução do conhecimento
+% analogamente ao que foi feito para prestadores, seria feito para utentes
+% e cuidados
+% -----------------
 
-evolucaoUtente(utente(Id,Nome,Idade,Morada)) :-
-    utentePerfeito(Id,Nome,Idade,Morada),
-    solucoes(Invariante, +utente(Id,Nome,Idade,Morada)::Invariante, Lista),
-    testa(Lista),
-    removeImpreciso(utente(Id,Nome,Idade,Morada)).
+% Extensão do predicado imperfeito: Prestador -> {V,F}
+% Indica se há conhecimento imperfeito relativo a um prestador
+imperfeito(prestador(Id,_,_,_)) :- incerto(prestador(Id)).
+imperfeito(prestador(Id,_,_,_)) :- impreciso(prestador(Id)).
 
-insercao( Termo ) :-
-    assert( Termo ).
-insercao( Termo ) :-
-    retract( Termo ),!,fail.
+% Extensão do predicado remocaoIncerto: Prestador -> {V,F}
+% Especialidade poderá ser incerta
+remocaoIncerto(prestador(Id,Nome,Especialidade,Instituicao)) :- incerto(prestador(Id)),
+        prestador(Id,Nome,nulo(incerto),Instituicao),
+        retract(prestador(Id,Nome,nulo(incerto),Instituicao)).
+% Instituição poderá ser incerta
+remocaoIncerto(prestador(Id,Nome,Especialidade,Instituicao)) :- incerto(prestador(Id)),
+        prestador(Id,Nome,Especialidade,nulo(incerto)),
+        retract(prestador(Id,Nome,Especialidade,nulo(incerto))).
+% Caso não haja conhecimento incerto, terá de haver impreciso
+remocaoIncerto(prestador(Id,_,_,_)) :- impreciso(prestador(Id)).
+
+% Extensão do predicado remocaoImperfeito: Prestador -> {V,F}
+% Permite remover todo o conhecimento imperfeito relativo a um prestador
+remocaoImperfeito(prestador(Id,_,_,_)) :- incerto(prestador(Id)),
+                                         retract(incerto(prestador(Id))).
+% Remove impreciso relativo a especialidade
+remocaoImperfeito(prestador(Id,Nome,Especialidade,Instituicao)) :-
+    impreciso(prestador(Id)),
+    retract(excecao(prestador(Id,Nome,Especialidade_1,Instituicao))),
+    remocaoImperfeito(prestador(Id,Nome,Especialidade,Instituicao)).
+% Remove impreciso relativo a instituição
+remocaoImperfeito(prestador(Id,Nome,Especialidade,Instituicao)) :-
+    impreciso(prestador(Id)),
+    retract(excecao(prestador(Id,Nome,Especialidade,Instituicao_1))),
+    remocaoImperfeito(prestador(Id,Nome,Especialidade,Instituicao)).
+% Remove anotação de impreciso
+remocaoImperfeito(prestador(Id,_,_,_)) :- impreciso(prestador(Id)),
+    retract(impreciso(prestador(Id))).
+
 
 teste( [] ).
 teste( [R|LR] ) :-
     R,
     teste( LR ).
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extens�o do predicado que permite a involucao do conhecimento
-involucao( T ) :- T,
-                solucoes(I, -T::I, LInv),
-                remocao(T),
-                teste(LInv).
-
-remocao(T) :- retract(T).
-remocao(T) :- assert(T), !, fail.
-
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensão do predicado e: Resposta 1, Resposta 2, Valor de Verdade -> {V,F}
+e(verdadeiro,verdadeiro,verdadeiro).
+e(verdadeiro,desconhecido,desconhecido).
+e(desconhecido,verdadeiro,desconhecido).
+e(falso,_,falso).
+e(_,falso,falso).
+
+% Extensão do predicado ou: Resposta 1, Resposta 2, Valor de Verdade -> {V,F}
+ou(verdadeiro,_,verdadeiro).
+ou(_,verdadeiro,verdadeiro).
+ou(falso,falso,falso).
+ou(X,desconhecido,desconhecido) :- nao(X == verdadeiro).
+ou(desconhecido,X,desconhecido) :- nao(X == verdadeiro).
+
 % Extensao do meta-predicado si: Questao,Resposta -> {V,F}
-%                            Resposta = { verdadeiro,falso,incerto,impreciso, desconhecido }
+%                            Resposta = { verdadeiro,falso,desconhecido }
+
+si( e(Q1,Q2) , Resposta ) :- si(Q1 , R1), si(Q2, R2), e(R1,R2,Resposta), !.
+si( ou(Q1,Q2) , Resposta) :- si(Q1 , R1), si(Q2, R2), ou(R1,R2,Resposta), !.
 
 si( Questao,verdadeiro ) :-
     Questao.
